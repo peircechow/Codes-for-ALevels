43abed1a81a154095a25362e01db3b2eb4cd6bdb
Peirce Chow Zheng Jie <chow.zhengjie.peirce@dhs.sg>
2017-07-21 11:17:34 +0800

    Update baaaasics.py

--- baaaasics.py
+++ baaaasics.py
@@ -13,3 +13,15 @@ def validateDate():
     except ValueError:
         correctDate = False
     print(str(correctDate))
+
+def diffDates(): # takes the difference of dates
+    import datetime as d
+    d1 = d.datetime(2017,1,20) # year month day
+    d2 = d.datetime(2000,1,30) 
+    print((d1-d2).days) # d.timedelta(days)
+
+def XDaysAfter():
+    import datetime as d
+    d1 = d.datetime(2017,1,20)
+    daysAfter = 90
+    print("{} after {} is {}".format(daysAfter, d1, d1+d.timedelta(daysAfter))
