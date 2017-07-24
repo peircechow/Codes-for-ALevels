9051279c08555e37d81bfc3b2b4413ba9b870f0b
Peirce Chow Zheng Jie <chow.zhengjie.peirce@dhs.sg>
2017-07-24 19:22:28 +0800

    Update SortAndSearch.py

--- SortAndSearch.py
+++ SortAndSearch.py
@@ -11,51 +11,40 @@ def binarySort2(A): # normal implementation
         return [A[len(A)//2]] + binarySort2(A[:len(A)//2]) +  binarySort2(A[len(A)//2+1 :])
 
 
-def quicksortIterative(A):
-
-    def partition(A): # takes in an unsorted array and return less equal more
-        less = []
-        equal = []
-        more = []
+def qsort(A):
+    def part(A):
+        less,equal,more = [],[],[]
         pivot = A[0]
         for item in A:
             if item<pivot:
                 less.append(item)
-            elif item==pivot:
+            elif item == pivot:
                 equal.append(item)
             else:
                 more.append(item)
-        return less,equal,more
+        return more,equal,less
+    
+    stack = [arr for arr in part(A)]
+    sortedArray = []
+    while stack != []:
+        array = stack.pop()
+        
+        if len(array) == 1: # [3] or [4] that kind
+            #print(array[0])
+            sortedArray.append(array[0])  # remove so dont have to sort
+        elif array == []:
+            pass
+        else:
+            moreEqualLess = part(array)
+            for i in range(len(moreEqualLess)):
+                subArr = moreEqualLess[i]
+                if i == 1:
+                    for item in subArr:
+                        stack.append([item])
+                else:
+                    stack.append(subArr)
+    return sortedArray
 
-    s = Stack()
-    A = [6,2,6,3,4,6,9,5,3,2,34]
-    less, equal, more = partition(A)
-    s.push(more)
-    s.push(equal)
-    s.push(less)
-    out = []
-
-    while not s.isEmpty(): #your keep doing until your stack is empty
-        if len(s.peek()) <= 1:
-            for i in s.peek():
-                out.append(i)
-            s.pop() # remove that element
-
-        else: # need to sort this array
-            unsorted = s.pop()
-            lessEqualMore = partition(unsorted)
-            moreEqualLess = lessEqualMore[::-1]
-            for i in range(len(moreEqualLess)): # more equal less
-                array = moreEqualLess[i]
-                if array != []:
-                    #if isEqual(array):
-                    if i == 1: # array of pivot ie all equal
-                        for item in array:
-                            s.push([item])
-                    else:
-                        s.push(array)
-
-    print(out)
 
 
         
