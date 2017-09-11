63388f2387dd0b3b145e9060a18d06ae0e0e60d9
Peirce Chow Zheng Jie <chow.zhengjie.peirce@dhs.sg>
2017-09-11 23:06:35 +0800

    Update SortAndSearch.py

--- SortAndSearch.py
+++ SortAndSearch.py
@@ -1,10 +1,11 @@
 def binarySort1(A): # one liner implementation
-    # accepts a sorted array and sort in the order to be inserted into the BST
+    # accepts a sorted array and sort in the order to be inserted into the BST (to make it balanced)
     # recursively similar to quicksort
     # root + left + right
+    # note: please do not use long one-linears for exam as it is not very readable
     return [A[len(A)//2]] + binarySort1(A[:len(A)//2]) + binarySort1(A[len(A)//2+1 :]) if A != [] else []
     
-def binarySort2(A): # normal implementation
+def binarySort2(A): # normal implementation (conditions same as above)
     if A == []:
         return []
     else:
@@ -32,13 +33,13 @@ def quicksortIterative(A):
         if len(array) == 1: # [3] or [4] that kind
             #print(array[0])
             sortedArray.append(array[0])  # remove so dont have to sort
-        elif array == []:
-            pass
-        else:
+        #elif array == []:
+        #    pass
+        elif array != []: # can ignore []
             moreEqualLess = part(array)
             for i in range(len(moreEqualLess)):
                 subArr = moreEqualLess[i]
-                if i == 1:
+                if i == 1: # pivot list (index 1) might contain duplicates
                     for item in subArr:
                         stack.append([item])
                 else:
